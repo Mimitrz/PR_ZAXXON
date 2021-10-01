5b56414bd0e04cdb113aca7199423fa19c674595
Maya Pardo de Santayana <mimi23m2002@gmail.com>
2021-10-01 18:00:50 +0200

    Movimiento de la nave y restricion
    La restricción es solo del eje x a la derecha

--- PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/Movimiento.cs
+++ PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/Movimiento.cs
@@ -8,7 +8,7 @@ public class Movimiento : MonoBehaviour
 
     [SerializeField] float speed;
     [SerializeField] float rotationspeed=100f;
-    
+ 
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,10 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float posX = transform.position.x;
+
+
+        //Movimientos y rotacion
         float desplX = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * desplX, Space.World);
         float desplY = Input.GetAxis("Vertical");
@@ -26,7 +30,12 @@ public class Movimiento : MonoBehaviour
         float desplR = Input.GetAxis("Rotacion");
         transform.Rotate(0f, 0f, desplR * Time.deltaTime * -rotationspeed);
 
-        
+        //Limites
+        if (posX > 10f && desplX > 0f)
+        {
+            transform.position = new Vector3 (posX, transform.position.y, transform.position.z);
+
+        }
       
     
     
