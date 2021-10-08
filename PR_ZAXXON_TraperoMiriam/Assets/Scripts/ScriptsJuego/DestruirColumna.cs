eac5f2740f1f030e423f03dba794df85a2c3e331
Miriam Trapero <mimi23m2002@gmail.com>
2021-10-08 17:01:25 +0200

    Destruir obstaculo
    Los obstáculos se destruyen cuando pasan una distancia

--- PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/DestruirColumna.cs
+++ PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/DestruirColumna.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientoColumna : MonoBehaviour
+public class DestruirColumna : MonoBehaviour
 {
-    float naveSpeed;
-    
-   
+    float destroyPosition = -12f;
+
     // Start is called before the first frame update
     void Start()
     {
-        naveSpeed = 30f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.back * Time.deltaTime * naveSpeed);
+     
+        if (transform.position.z <= destroyPosition)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
--- PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/Instanciar.cs
+++ PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/Instanciar.cs
@@ -12,27 +12,34 @@ public class Instanciar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        intervalo = 1f;
+        intervalo = 0.5f;
 
         StartCoroutine("CrearColumna");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-       
+      
     }
 
     IEnumerator CrearColumna()
     {
         while (true)
         {
-            float randomX = Random.Range(-10f, 10f);
+            float randomX = Random.Range(-15f, 15f);
             Vector3 newPos = new Vector3(randomX, instantiatePos.position.y, instantiatePos.position.z);
             Instantiate(columna, newPos, Quaternion.identity);
 
             yield return new WaitForSeconds(intervalo);
         }
+
+
     }
+
+
+  
+
+    
 }
