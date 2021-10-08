906de4c26b74b08d20a5d7e645ab9f533ba3c0a8
Miriam Trapero <mimi23m2002@gmail.com>
2021-10-08 19:14:42 +0200

    Variación de distancia
    Para que la distancia entre obstáculos sea la misma independientemente de la velocidad

--- PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/Instanciar.cs
+++ PR_ZAXXON_TraperoMiriam/Assets/Scripts/ScriptsJuego/Instanciar.cs
@@ -7,12 +7,18 @@ public class Instanciar : MonoBehaviour
     [SerializeField] GameObject columna;
     [SerializeField] Transform instantiatePos;
     float intervalo;
-    
-    
+    [SerializeField] float distance;
+
+    //Acceder al script con las variables generales
+    InitGame inicioJuego;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        intervalo = 0.5f;
+        //intervalo = 0.5f;
+        distance = 30f;
+        inicioJuego = GameObject.Find("InicioDeJuego").GetComponent<InitGame>();
 
         StartCoroutine("CrearColumna");
         
@@ -26,9 +32,16 @@ public class Instanciar : MonoBehaviour
 
     IEnumerator CrearColumna()
     {
+        
+
         while (true)
         {
-            float randomX = Random.Range(-15f, 15f);
+            float speed;
+            speed = inicioJuego.juegoSpeed;
+            intervalo = distance / speed;
+            print(intervalo);
+
+            float randomX = Random.Range(-20f, 20f);
             Vector3 newPos = new Vector3(randomX, instantiatePos.position.y, instantiatePos.position.z);
             Instantiate(columna, newPos, Quaternion.identity);
 
